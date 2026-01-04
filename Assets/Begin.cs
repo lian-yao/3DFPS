82583533d4304eb2d183508cd861f76f47e5b8cd
Kalpas123 <209745259@qq.com>
2026-01-04 19:49:23 +0800

    角色添加操控

---
+++ Assets/Begin.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+// 类名需与文件名一致（若文件名为bigin.cs，类名应为bigin）
+public class Begin : MonoBehaviour
+{
+    // 方法需用大括号闭合
+    public void OnLoginButtonClick()
+    {
+        SceneManager.LoadScene("test");
+    }
+}
